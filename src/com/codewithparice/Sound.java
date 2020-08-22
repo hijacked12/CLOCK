66800ec09f63b7114894a058e29b128866cd47f8
Joe <olarindejoseph1997@gmail.com>
2020-08-22 16:46:52 +0800

    First Project

---
+++ src/com/codewithparice/Sound.java
@@ -0,0 +1,34 @@
+package com.codewithparice;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by Nesmy Parice BAKALA on 06/07/2020.
+ */
+public class Sound {
+
+    private AudioInputStream audioStream;
+    private Clip audioClip;
+
+    public Sound(String song) throws IOException, UnsupportedAudioFileException, LineUnavailableException {
+        File audioFile = new File(song);
+        audioStream = AudioSystem.getAudioInputStream(audioFile);
+
+        AudioFormat format = audioStream.getFormat();
+        DataLine.Info info = new DataLine.Info(Clip.class, format);
+        audioClip = (Clip) AudioSystem.getLine(info);
+    }
+
+    public void playAudio() throws IOException, LineUnavailableException {
+        audioClip.open(audioStream);
+        audioClip.start();
+        audioClip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    public void stopAudio() throws IOException {
+        audioClip.close();
+        audioStream.close();
+    }
+}
